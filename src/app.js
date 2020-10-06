5b86e785f3441125d1d0ce40c538ebf4fb98cfc8
NettoVieira <joao.fvneto@hotmail.com>
2020-10-05 21:16:01 -0300

    creating routes for inclusion and alteration of repositories

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require('uuidv4');
 
 // const { v4: uuid } = require('uuid');
 
@@ -12,18 +13,53 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const {title, url, techs, likes = 0} = request.body;
+
+  const repository = {id: uuid(), title, url, techs, likes};
+
+  repositories.push(repository);
+
+  return response.json([ repository ]);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+   
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json( { error: 'repository not found' } );
+  };
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+ 
 });
 
+
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repositories.splice(repositories.indexOf(id), 1);
+
+  return response.status(200).json(repositories);
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
